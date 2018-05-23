dc2166a23649f5255d820598c853bbea530f9b57
Глазунов Сергей <glazunov@keyintegrity.com>
2018-05-23 11:38:33 +0300

    add blacklist and whitelist

--- user_agreement/middleware.py
+++ user_agreement/middleware.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 
+from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.utils.cache import add_never_cache_headers
@@ -11,7 +12,22 @@ agreement_url = reverse('user_agreement')
 
 
 class UserAgreementMiddleware(MiddlewareMixin):
+    def path_in_list(self, path, url_list):
+        result = [url for url in url_list if path.startswith(url)]
+        return True if result else False
+
+    def path_in_blacklist(self, path):
+        black_list = getattr(settings, 'AGREEMENT_URLS_BLACKLIST', [])
+        return self.path_in_list(path, black_list)
+
+    def path_in_whitelist(self, path):
+        white_list = getattr(settings, 'AGREEMENT_URLS_WHITELIST', [])
+        return self.path_in_list(path, white_list)
+
     def skip_agreement_checking(self, request):
+        if self.path_in_whitelist(request.path_info):
+            return True
+
         if request.method != 'GET':
             return True
 
@@ -31,13 +47,13 @@ class UserAgreementMiddleware(MiddlewareMixin):
             return True
 
     def process_request(self, request):
-        if self.skip_agreement_checking(request):
-            return
+        if self.path_in_blacklist(request.path_info):
+            if not self.skip_agreement_checking(request):
+                redirect_to = request.path_info
+                querystring = request.META['QUERY_STRING']
+                if querystring:
+                    redirect_to += '?' + querystring
+                response = HttpResponseRedirect(agreement_url + '?redirect_to={}'.format(redirect_to))
+                add_never_cache_headers(response)
+                return response
 
-        redirect_to = request.path_info
-        querystring = request.META['QUERY_STRING']
-        if querystring:
-            redirect_to += '?' + querystring
-        response = HttpResponseRedirect(agreement_url + '?redirect_to={}'.format(redirect_to))
-        add_never_cache_headers(response)
-        return response
--- user_agreement/tests/urls.py
+++ user_agreement/tests/urls.py
@@ -12,9 +12,19 @@ def some_page(request):
     return HttpResponse('some_page')
 
 
+def some_page_in_blacklist(request):
+    return HttpResponse('some_page_in_blacklist')
+
+
+def some_page_in_whitelist(request):
+    return HttpResponse('some_page_in_whitelist')
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', index, name='index'),
     url(r'^some_page/$', some_page, name='some_page'),
+    url(r'^some_page/in_blacklist/$', some_page_in_blacklist, name='some_page_in_blacklist'),
+    url(r'^some_page/in_whitelist/$', some_page_in_whitelist, name='some_page_in_whitelist'),
     url(r'^user_agreement/', include('user_agreement.urls')),
 ]
