2975acd8bf6b0491df2e6588bf6c5c1ab5bbff38
Artyom Opanchuk <artyom.opanchuk@gmail.com>
2020-03-27 16:22:26 +0300

    README

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 django-user-agreement
+=====================
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import io
 import os
 from setuptools import setup, find_packages
 
-with io.open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+with io.open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
 
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
