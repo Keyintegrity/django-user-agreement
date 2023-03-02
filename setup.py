9f785bd21783ccff1149b746333f030c8c5404cc
Artyom Opanchuk <subzero666@list.ru>
2023-03-02 18:53:53 +0300

    Add support for Django 3.2

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-user-agreement',
-    version='0.1.1',
+    version='0.2.0',
     packages=['user_agreement'],
     include_package_data=True,
     description='django-user-agreement',
     long_description=README,
     install_requires=[
-        'Django>=1.11,<3.0',
+        'Django>=2.2,<4.0',
     ],
     url='https://github.com/Keyintegrity/django-user-agreement',
     author='keyintegrity',
@@ -27,10 +27,11 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
 )
--- user_agreement/apps.py
+++ user_agreement/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class UserAgreementConfig(AppConfig):
     name = 'user_agreement'
+    default_auto_field = 'django.db.models.AutoField'
